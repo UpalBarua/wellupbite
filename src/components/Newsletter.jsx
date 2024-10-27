b0536790ab1d67910000bb8666228dd45f18ecc9
wellupweb <wellupweb@gmail.com>
2024-10-27 16:25:47 +0600

    added newsletter and footer section

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -2,18 +2,19 @@ import React from "react";
 
 export default function Newsletter() {
   return (
-    <section className="container mx-auto px-2">
-      <div>
-        <h2>Deliciousness to your inbox</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam,
-          consequatur.
-        </p>
-        <form>
-          <input type="text" />
-          <button>JOIN</button>
-        </form>
-      </div>
+    <section className="mx-auto px-2 bg-primary/25 flex flex-col items-center justify-center text-center h-[22rem] gap-6">
+      <h2 className="font-lobster text-6xl">Deliciousness to your inbox</h2>
+      <p className="text-lg leading-relaxed max-w-[60ch]">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam,
+        consequatur.
+      </p>
+      <form className="join">
+        <input
+          className="input input-bordered join-item w-[20rem]"
+          placeholder="Example@email.com"
+        />
+        <button className="btn join-item btn-primary">Subscribe</button>
+      </form>
     </section>
   );
 }
